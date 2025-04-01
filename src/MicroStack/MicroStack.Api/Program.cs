b44d364311b306badbbbc846898928ebc1bb8a34
Amit P Naik <amit.naik8103@gmail.com>
2025-04-01 23:11:40 +0530

    feat: sample application

---
+++ src/MicroStack/MicroStack.Api/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:7150"; // Identity Server URL
+        options.Audience = "api1"; // Must match the configured API scope
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
