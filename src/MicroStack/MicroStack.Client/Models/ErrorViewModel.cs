eaecce0cf7f4c005d99a50e431abc0e1ddd6ac19
Amit P Naik <amit.naik8103@gmail.com>
2025-04-06 20:44:56 +0530

    feat: mvc application

---
+++ src/MicroStack/MicroStack.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroStack.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
