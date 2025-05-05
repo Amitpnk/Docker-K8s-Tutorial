4e0373a4f02ca056c635592995c7e6db1edc7265
Amit P Naik <amit.naik8103@gmail.com>
2025-05-05 19:53:13 +0530

    feat: enabling swagger and scalar

--- src/WeatherService/Program.cs
+++ src/WeatherService/Program.cs
@@ -12,14 +12,11 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
