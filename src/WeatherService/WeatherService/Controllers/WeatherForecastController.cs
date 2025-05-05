15e881fb594c29de31705e0a7fa1d93cab404967
Amit P Naik <amit.naik8103@gmail.com>
2025-05-05 19:18:38 +0530

    feat: adding weather service api

---
+++ src/WeatherService/WeatherService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        logger.LogInformation("GetWeatherForecast endpoint called.");
+
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        logger.LogInformation("Generated {Count} weather forecasts.", forecasts.Length);
+
+        return forecasts;
+    }
+}
