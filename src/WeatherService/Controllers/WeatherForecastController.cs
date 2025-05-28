7589e31366a6381b56c6c44c976544e188f48579
Amit P Naik <amit.naik8103@gmail.com>
2025-05-28 23:02:54 +0530

    feat: using globalusing

--- src/WeatherService/Controllers/WeatherForecastController.cs
+++ src/WeatherService/Controllers/WeatherForecastController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace WeatherService.Controllers;
 
 [ApiController]
---
+++ src/WeatherService/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Scalar.AspNetCore;
